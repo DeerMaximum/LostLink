d57ab765b054352055ed44d23c686d21131e4460
wielgola <39047784+wielgola@users.noreply.github.com>
2024-11-03 15:03:53 +0100

    Added Graph API authentication and file download

---
+++ Application File History AI/main.py
@@ -0,0 +1,14 @@
+def main():
+    print("Started File History AI:")
+    #print("--- authenticate for graph api")
+    #print("--- find and download files via api ---")
+    #print("--- load files ---")
+    #print("--- create embeddings ---")
+    #print("--- cluster files ---")
+    #print("--- create history for clusters")
+
+
+
+
+if __name__ == "__main__":
+    main()
