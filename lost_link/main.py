01f5e95ccfb0234963fd1cd0e75c69b56bd011ad
DeerMaximum <git983456@parabelmail.de>
2024-11-15 23:57:45 +0100

    add vector db and embedding model

--- lost_link/main.py
+++ lost_link/main.py
@@ -1,5 +1,10 @@
+import os
+
+from langchain_community.embeddings import LlamaCppEmbeddings
+
 from db import DB
 import args
+from langchain_chroma import Chroma
 
 from lost_link.ai.models import ModelManager
 from lost_link.dir_manager import DirManager
@@ -23,10 +28,19 @@ def main():
         return
 
     model_manager = ModelManager(dir_manager.get_model_dir())
+    vector_db = Chroma(persist_directory=dir_manager.get_vector_db_dir())
 
     print("Started File History AI:")
+
     print("Prepare ai models")
     model_manager.init_models()
+
+    embeddings_model = LlamaCppEmbeddings(
+        model_path=os.path.join(dir_manager.get_model_dir(), model_manager.get_embedding_model_filename()),
+        n_gpu_layers=-1,  # Set to 0 for only cpu
+        verbose=run_debug
+    )
+
     #print("--- authenticate for graph api")
     #print("--- find and download files via api ---")
     #print("--- load files ---")
